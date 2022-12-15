1b9bfc725baff71f64581876bc96c993bd30b248
Sinan <sinandavid@hotmail.de>
2022-12-15 22:45:24 +0100

    refactored type structure of NeuralNetwork mod

--- src/NeuralNetwork.hs
+++ src/NeuralNetwork.hs
@@ -2,19 +2,27 @@ module NeuralNetwork
   ( NeuralNetwork(..)
   ) where
 
+import Prelude
 import           NeuralNetwork.Matrix
 
 data NeuralNetwork =
   NeuralNetwork Input Hidden Output
+  deriving Show
+
+type InputNodes = Int
+
+type HiddenNodes = Int
+
+type OutputNodes = Int
 
 newtype Layer =
-  Layer (Matrix Int)
+  Layer (Matrix Int) deriving Show
 
 newtype Input =
-  Input Layer
+  Input Layer deriving Show
 
 newtype Hidden =
-  Hidden [Layer]
+  Hidden [Layer] deriving Show
 
 newtype Output =
-  Output Layer
+  Output Layer deriving Show
